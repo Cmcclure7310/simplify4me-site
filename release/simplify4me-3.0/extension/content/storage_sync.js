81adbfdca6241a09198c96d61a952e91eb5ffd06
Cmcclure7310 <cmcclure7310@gmail.com>
2026-09-10 19:36:57 -0400

    Remove unused settings bridge

--- release/simplify4me-3.0/extension/content/storage_sync.js
+++
@@ -1,8 +0,0 @@
-(() => {
-  'use strict';
-  if (!chrome?.storage?.onChanged) return;
-  chrome.storage.onChanged.addListener((_changes, areaName) => {
-    if (areaName !== 'local') return;
-    try { chrome.runtime.sendMessage({ type: 'S4M_STORAGE_CHANGED' }).catch(() => {}); } catch {}
-  });
-})();
